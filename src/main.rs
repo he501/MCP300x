637bd26d49f6688bcf1b843408becbe93adea7e1
he501 <81799557+he501@users.noreply.github.com>
2021-07-28 18:36:47 +0000

    read num

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use rppal::spi;
 fn main() {
     println!("Hello, world!");
-    analog_read(3,spi::Bus::Spi1);
-    // let val=analog_read(0,spi::Bus::Spi1);
+    let val=analog_read(0,spi::Bus::Spi0);
+    println!("val={}",val);
 }
 
 // fn analog_read(ch:u8,bus_no:rppal::spi::Bus)->Result<i32,String>{
-fn analog_read(ch:u8,bus_no:spi::Bus){
-
+fn analog_read(ch:u8,bus_no:spi::Bus)->i32{
     println!("ok");
-    let mut r_buf:[u8;2]=[0;2];
+    let mut r_buf:[u8;3]=[0;3];
     let w_buf:[u8;3]=[0x01,(0x01<<7)|(ch&0x07)<<4,0x00];
     println!("{:?}",w_buf);
-    let spi=spi::Spi::new(bus_no, spi::SlaveSelect::Ss0, 100_000, spi::Mode::Mode0).unwrap();
+    let spi=spi::Spi::new(bus_no, spi::SlaveSelect::Ss0, 50_000, spi::Mode::Mode0).unwrap();
     spi::Spi::transfer(&spi,&mut r_buf,&w_buf).unwrap();
     println!("{:?}",r_buf);
+    ((r_buf[1]as i32 & 0x03)<<8)|r_buf[2] as i32
 }
